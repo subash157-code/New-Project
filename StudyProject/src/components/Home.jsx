9d98d7ca85c19e18393c50eb833cdc9c213516b4
Subash Ravi <subashravi157@gmail.com>
2025-08-07 12:30:42 +0530

    Update Home.jsx

--- StudyProject/src/components/Home.jsx
+++ StudyProject/src/components/Home.jsx
@@ -80,7 +80,7 @@ const Home = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axios.post("http://localhost:5000/api/newuser", formData);
+      await axios.post("https://new-project-backend-hhl0.onrender.com/newuser", formData);
       setSubmitted(true);
       setFormData({ name: "", email: "", course: "", date: "", message: "" });
     } catch (err) {
